444aff458ef897357b4091dd65b0c1f8884e64e8
manhminno <manh6264@gmail.com>
2020-11-03 22:20:32 +0700

    Change database

--- controller/connectToDatabase.php
+++ controller/connectToDatabase.php
@@ -1,7 +1,7 @@
 <?php
 	//kết nối đến CSDL
-	// $conn = mysqli_connect("localhost", "root", "", "tro_tot_hn_db");
-	$conn = mysqli_connect("localhost", "root", "", "mysql");
+	$conn = mysqli_connect("localhost", "root", "", "tro_tot_hn_db");
+	// $conn = mysqli_connect("localhost", "root", "", "mysql");
 	if (!$conn) { //kiểm tra xem đã kết nối đến CSDL được chưa
 		die("Connection failed: " . mysqli_connect_error());
 	}
