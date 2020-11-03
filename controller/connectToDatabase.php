b25c24948f017b677343cc8c7c057a452cc4c7a6
manhminno <manh6264@gmail.com>
2020-11-03 23:44:04 +0700

    Update database

--- controller/connectToDatabase.php
+++ controller/connectToDatabase.php
@@ -1,7 +1,8 @@
 <?php
 	//kết nối đến CSDL
-	$conn = mysqli_connect("localhost", "root", "", "tro_tot_hn_db");
+	// $conn = mysqli_connect("localhost", "root", "", "tro_tot_hn_db");
 	// $conn = mysqli_connect("localhost", "root", "", "mysql");
+	$conn = mysqli_connect("localhost", "root", "", "nha_tro_db");
 	if (!$conn) { //kiểm tra xem đã kết nối đến CSDL được chưa
 		die("Connection failed: " . mysqli_connect_error());
 	}
