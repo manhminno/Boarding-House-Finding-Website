ffb249cf2ddc47e0dfcd1665783baed565d4d2dd
manhminno <manh6264@gmail.com>
2020-12-09 23:33:04 +0700

    Add show uploaded post in profile

--- TrangCaNhan.php
+++ TrangCaNhan.php
@@ -66,56 +66,17 @@
 					?>
 				</div>
 			</div>
-			<div class="col-lg-8 col-md-8 col-sm-7 col-xs-12">
-
-				<div class="col-xs-12" style="padding: 0px; font-size: 23px; color: green;">
-					Các căn phòng đã đăng
+			<!-- Phần hiển thị các tin bài -->
+			<div class="col-xs-12" id="room_main_content">
+				<div class="row" id="new_rooms">
+					
+					<!-- Hiển thị các phòng trọ -->
+					<?php
+						include('module/uploadRoom.php');
+					?>
+					
 				</div>
-
-				<div class="col-xs-12" style="border: solid 1px #d4d0d0; padding: 10px 0px; margin: 10px 0px; border-radius: 4px; height: 145px;">
-					<div class="row">
-						<div class="col-md-3 col-xs-4">
-							<img src="uploads/6.jpg" style="height: 125px; width: 100%; object-fit: scale-down; margin-left: 10px;">
-						</div>
-						<div class="col-md-9 col-xs-8">
-							<div class="row">
-								<div class="col-xs-12" style="font-size: 23px; overflow: hidden; height: 30px;">
-									Cho thuê phòng trọ khu vực mỹ đình gfdg fgfgfg fg fgf gfg fg fgf
-								</div>
-								<div class="col-xs-12" style="overflow: hidden; height: 60px;">
-									Phòng trọ kiể mới chung cư mini đầy đủ tiện nghi trang trí đẹp trang nhã có tủ đồ bàn ghế như nhà dân nhưng đéo muốn cho thuê fg fgfg fg fgfg f gfg fggfgfgrgrgjf gfdgo fdghjfdhg fhgjhfd gkhfdg fhgkjfd gjfdhgkfdkg fdhgk hfdjgh fdhgkjfd hgkhfdg hfdjg kfdhgkdfhgk
-								</div>
-								<div class="col-xs-12">
-									<button class="btn btn-default btn-sm pull-right" type="button">Xóa</button>
-									<button class="btn btn-default btn-sm pull-right" type="button">Sửa</button>
-								</div>
-							</div>
-						</div>						
-					</div>
-				</div>
-
-				<div class="col-xs-12" style="border: solid 1px #d4d0d0; padding: 10px 0px; margin: 10px 0px; border-radius: 4px; height: 145px;">
-					<div class="row">
-						<div class="col-md-3 col-xs-4">
-							<img src="uploads/2.jpg" style="height: 125px; width: 100%; object-fit: scale-down; margin-left: 10px;">
-						</div>
-						<div class="col-md-9 col-xs-8">
-							<div class="row">
-								<div class="col-xs-12" style="font-size: 23px; overflow: hidden; height: 30px;">
-									Cho thuê phòng trọ khu vực mỹ đình gfdg fgfgfg fg fgf gfg fg fgf
-								</div>
-								<div class="col-xs-12" style="overflow: hidden; height: 60px;">
-									Phòng trọ kiể mới chung cư mini đầy đủ tiện nghi trang trí đẹp trang nhã có tủ đồ bàn ghế như nhà dân nhưng đéo muốn cho thuê fg fgfg fg fgfg f gfg fggfgfgrgrgjf gfdgo fdghjfdhg fhgjhfd gkhfdg fhgkjfd gjfdhgkfdkg fdhgk hfdjgh fdhgkjfd hgkhfdg hfdjg kfdhgkdfhgk
-								</div>
-								<div class="col-xs-12">
-									<button class="btn btn-default btn-sm pull-right" type="button">Xóa</button>
-									<button class="btn btn-default btn-sm pull-right" type="button">Sửa</button>
-								</div>
-							</div>
-						</div>						
-					</div>
-				</div>
-
+			</div>
 
 			</div>
 		</div>
