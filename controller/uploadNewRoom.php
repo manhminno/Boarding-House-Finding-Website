202496bc06d230105ba3dfb6e0a8c3c0fbebb29f
manhminno <manh6264@gmail.com>
2020-12-17 22:34:17 +0700

    Add plugin facebook cmt and chatbox

--- controller/uploadNewRoom.php
+++ controller/uploadNewRoom.php
@@ -12,9 +12,10 @@
 
 		if(mysqli_query($conn, $sql_insert_dia_chi_phong_tro)) {
 			$last_id = mysqli_insert_id($conn);
-
+			date_default_timezone_set('Asia/Ho_Chi_Minh'); 
+			$today = date('Y-m-d H:i:s');
 			//Câu lệnh sql insert giá trị vào bảng gia_phong_tro
-			$sql_insert_gia_phong_tro = 'INSERT INTO gia_phong_tro(IDPhongTro, user_name, TieuDe, KieuPhong, KieuVeSinh, GiaChoThue, DienTich, GiaDien, GiaNuoc, DoiTuong, TienIch, MoTa) VALUE("' .$last_id. '", "' .$_SESSION['user_name']. '", "' .$_POST['title']. '", "' .$_POST['KieuPhong']. '", "' .$_POST['VeSinh']. '", "' .$_POST['price']. '", "' .$_POST['area']. '", "' .$_POST['electricPrice']. '", "' .$_POST['waterPrice']. '", "' .$_POST['doiTuong']. '", "' .$_POST['tienIch']. '", "' .$_POST['moTa']. '")';
+			$sql_insert_gia_phong_tro = 'INSERT INTO gia_phong_tro(IDPhongTro, user_name, TieuDe, KieuPhong, KieuVeSinh, GiaChoThue, DienTich, GiaDien, GiaNuoc, DoiTuong, TienIch, MoTa, ThoiGianDang) VALUE("' .$last_id. '", "' .$_SESSION['user_name']. '", "' .$_POST['title']. '", "' .$_POST['KieuPhong']. '", "' .$_POST['VeSinh']. '", "' .$_POST['price']. '", "' .$_POST['area']. '", "' .$_POST['electricPrice']. '", "' .$_POST['waterPrice']. '", "' .$_POST['doiTuong']. '", "' .$_POST['tienIch']. '", "' .$_POST['moTa']. '", "' .$today. '")';
 
 			mysqli_query($conn, $sql_insert_gia_phong_tro);
 
